a97a6e077b054341433f112af581b833add980ab
Sanjiv Yelthimar Shenoy <sanjiv.shenoy@gmail.com>
2020-07-26 06:50:39 +0530

    Readme files and comments added

--- Google Assistant Integration Code/Report_Flask_App/application.py
+++ Google Assistant Integration Code/Report_Flask_App/application.py
@@ -1,7 +1,9 @@
+#import the required files
 from flask import Flask, render_template
 import pyrebase
 import os
 
+#the configuration data required to connect to the firebase realtime database
 keys = {
     "apiKey": "AIzaSyDZKPNKXkW_R8yUog4onZNm1VDVQuwTPcc",
     "authDomain": "aashray-3d07c.firebaseapp.com",
@@ -13,19 +15,21 @@ keys = {
     "measurementId": "G-WHC1XZ7MFQ"
 }
 
+#Port configuration for the app
 port = int(os.getenv('PORT', 8000))
 
 app = Flask(__name__)
 
+#Initialising the database
 firebase = pyrebase.initialize_app(keys)
-
 db = firebase.database()
 
+#Getting the patients to render from the database
 patients = db.child("Patient").get()
 patients = patients.val()
 patients = patients[1:]
 
-
+#Host the patient data on the '/' route
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template("index.html", patients=patients)
